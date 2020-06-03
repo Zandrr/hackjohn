f07028392b3b38ddd1dd383d9d27a6662004193e
EC2 Default User <ec2-user@ip-172-31-22-3.us-east-2.compute.internal>
2020-06-03 20:30:22 +0000

    changed csv generation to use its own instance of space_df instead of changing space_df columns. Might be cause of bug...

--- hackjohn.py
+++ hackjohn.py
@@ -6,7 +6,7 @@ import pandas
 from pkg_resources import parse_version
 
 # Minimum number of available spaces
-spaces = 1
+spaces = 2
 
 # Comment out trailheads you'd like to start from
 exclude = [
@@ -19,7 +19,6 @@ exclude = [
 
 # Dates you'd like to start on (inclusive of end date)
 dates = pandas.date_range(start="2020-06-01", end="2020-10-05", freq="D")
-dates
 
 # Write output to this file. If the generated output is identical to
 # the existing output at this path, suppress notification. To disable
@@ -55,7 +54,6 @@ def get_trailhead_df():
     )
     wide_df = wide_df.iloc[:, :6]
 
-
     trailhead_df = (
         wide_df.melt(id_vars="Date", var_name="Trailhead", value_name="Spaces")
         .dropna()
@@ -68,13 +66,13 @@ def get_trailhead_df():
 
 
 yose_response, trailhead_df = get_trailhead_df()
-trailhead_df.head(2)
 
 # Extract report date
 try:
     match = re.search(r"Report Date: ([0-9/]+)", yose_response.text)
     report_date = match.group(1)
     report_date = pandas.to_datetime(report_date, dayfirst=False)
+    print("Report date is, ", report_date)
 except Exception:
     report_date = yose_response.headers["Date"]
     report_date = pandas.to_datetime(report_date, utc=True)
@@ -83,32 +81,30 @@ report_date = report_date.date().isoformat()
 space_df = trailhead_df.query(
     "Date in @dates and Spaces >= @spaces and Trailhead not in @exclude"
 )
-space_df
+# space_df
 
 space_str = "NO VACANCY" if space_df.empty else space_df.to_string(index=False)
-text = f"""Spaces available as of {report_date}:
+text = f"{space_str}"
 
-{space_str}
-
-According to {yose_response.url}
-Yosemite Reservations: 209-372-0740 (Monday–Friday 9:00am–4:30pm)
-"""
 print(text)
 
 #create csv so we can format in html
-space_df.columns = ['Date', 'Trailhead', 'Spaces']
+csv_space_df = space_df
+csv_space_df.columns = ['Date', 'Trailhead', 'Spaces']
 csv_data = "NO VACANCY" if space_df.empty else space_df.to_csv(r'hackcsv.csv', index=False)
 
 html_file = pandas.read_csv('hackcsv.csv')
 html_file = html_file.to_html()
-phone_hours = """
-According to https://www.nps.gov/yose/planyourvisit/fulltrailheads.htm,
+phone_hours = f"""
+
+According to https://www.nps.gov/yose/planyourvisit/fulltrailheads.htm
+
 Yosemite Reservations: 209-372-0740 (Monday–Friday 9:00am–4:30pm)
+
 Apply at https://yosemite.org/yosemite-wilderness-permit-request-form/
 """
 
-
-# Detect if output_path has changed. If so, rewrite output.
+# Detect if output_path has changed. If so, rewrite output. Also write previous text to previous_output_path so we can find the diff
 output_has_changed = True
 if output_path:
     output_path = pathlib.Path(output_path)
@@ -121,7 +117,6 @@ print(f"output has changed: {output_has_changed}")
 
 # determine whether to notify
 notify = not space_df.empty and output_has_changed and min_report_date <= report_date
-
 
 
 #Zapier
